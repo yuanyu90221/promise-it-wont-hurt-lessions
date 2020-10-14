d29c710091c741d8c32207f995525ae23455b5ed
jsonLiang <yuanyu90221@gmail.com>
2020-10-14 18:03:52 +0800

    add optReject.js

---
+++ optReject.js
@@ -0,0 +1,9 @@
+'use strict';
+const promise = new Promise(function(fulfill, reject){
+  fulfill('I FIRED');
+  reject(new Error('I DID NOT FIRE'));
+});
+function onReject(error) {
+  console.log(error.message);
+}
+promise.then(console.log, onReject);
