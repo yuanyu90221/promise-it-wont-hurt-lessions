955c64410d3bad2fc21fb7191ce9f190a7267c1d
jsonLiang <yuanyu90221@gmail.com>
2020-10-14 17:51:52 +0800

    add rejected.js

---
+++ rejected.js
@@ -0,0 +1,10 @@
+'use strict'
+var promise = new Promise(function(fulfill, reject) {
+ setTimeout(()=>{
+  reject(new Error('REJECTED!'));
+ }, 300);
+})
+function onReject (error) {
+ console.log(error.message);
+}
+promise.then(null, onReject);
