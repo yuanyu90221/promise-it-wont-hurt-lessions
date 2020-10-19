f61545492c66d60eefbe714369165a5a464cc872
jsonLiang <yuanyu90221@gmail.com>
2020-10-19 12:30:44 +0800

    another-solution

--- promise-chain.js
+++ promise-chain.js
@@ -1,2 +1,5 @@
 'use strict';
-first().then(second).then(console.log);
+//first().then(second).then(console.log);
+const firstPromise = first();
+const secondPromise = firstPromise.then(second).then(console.log);
+
