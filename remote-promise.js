48324e0ca07a08e9ae290dfb90c636562712b6f0
jsonLiang <yuanyu90221@gmail.com>
2020-10-20 17:34:54 +0800

    offical soluction

--- remote-promise.js
+++ remote-promise.js
@@ -1,25 +1,27 @@
 'use strict';
-const http = require('http');
-const fetchResult = (url) => {
-  return new Promise((resolve, reject) => {
-    http.get(url, (res) => {
-     let {statusCode} = res;
-     if (statusCode!==200) {
-       reject(new Error('request error'))
-     }
-     let rawData = '';
-     res.on('data', (chunk)=> { rawData += chunk})
-     res.on('end', () => {
-       try {
-        const parseData = JSON.parse(rawData);
-        resolve(parseData);
-       } catch (e) {
-         resolve(rawData);
-       }
-     }).on('error', e => reject(e));
-    })
-  });
-}
-fetchResult('http://localhost:7000').then(id => {
-  return fetchResult(`http://localhost:7001/${id}`)
-}).then(console.log);
+const http = require('q-io/http');
+// const fetchResult = (url) => {
+//   return new Promise((resolve, reject) => {
+//     http.get(url, (res) => {
+//      let {statusCode} = res;
+//      if (statusCode!==200) {
+//        reject(new Error('request error'))
+//      }
+//      let rawData = '';
+//      res.on('data', (chunk)=> { rawData += chunk})
+//      res.on('end', () => {
+//        try {
+//         const parseData = JSON.parse(rawData);
+//         resolve(parseData);
+//        } catch (e) {
+//          resolve(rawData);
+//        }
+//      }).on('error', e => reject(e));
+//     })
+//   });
+// }
+http.read('http://localhost:7000').then(id => {
+  return http.read(`http://localhost:7001/${id}`)
+}).then(json => console.log(JSON.parse(json)))
+.then(null, console.error)
+.done();
